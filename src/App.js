9bf9a28e51376e65ed252415268b6984daf7a5ef
sckcs15 <100522412+sckcs15@users.noreply.github.com>
2023-04-04 00:01:40 +0900

    create project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [loading, setLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const getMovies =async ()=>{
+    const data = await (await fetch("")).json();
+    
+  }
+  useEffect(()=>{
+  });
+ return(
+  <div>
+
+  </div>
+ )
 }
 
 export default App;
